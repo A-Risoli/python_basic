4b0465ffc2a19a0c40d7784b10664affc2ac9ea6
POLITO\s192867 <POLITO\s192867@ladix29>
2015-03-16 16:26:31 +0100

    es7 ok

--- p-basic/es7.py
+++ p-basic/es7.py
@@ -3,15 +3,21 @@ Created on 16/mar/2015
 
 @author: antonio
 '''
-
+n={}
 die={'age':19, 'instrument':'trombone'}
 nel={'age':21, 'instrument':'clarinet'}
 sal={'age':18, 'instrument':'trombone'}
 fra={'age':40, 'instrument':'euphonium'}
 ant={'age':22, 'instrument':'trombone'}
 mik={'age':21, 'instrument':'sax'}
-people={'die':'Diego','nel':'Nello','sal':'Salvatore', 'fra':'francesco','mik':'Michele'}
+people={'Diego':die,'Nello':nel,'Salvatore':sal,'francesco':fra,'Michele':mik, 'Antonio':ant}
 
-ins=raw_input("find_instrument ")
+s=raw_input("find_instrument ")
+
+for a,b in people.items():
+    if s==b['instrument']:
+        n[a]=b
+print n
+
 
 
