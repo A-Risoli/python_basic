3a58cd3541ffaa9a7085708af3ef4f6bf70a7947
A-Risoli <flicten93@libero.it>
2015-03-16 13:04:09 +0100

    mancano 7 e 8

--- p-basic/es6.py
+++ p-basic/es6.py
@@ -3,3 +3,16 @@ Created on 16/mar/2015
 
 @author: antonio
 '''
+
+i=['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] 
+j=[]
+for s in i:
+    if s[0]=='x':
+        j.append(s)
+        i.remove(s)
+i.sort()
+j.sort()
+
+j+=i
+print j
+        
---
+++ p-basic/es7.py
@@ -0,0 +1,17 @@
+'''
+Created on 16/mar/2015
+
+@author: antonio
+'''
+
+die={'age':19, 'instrument':'trombone'}
+nel={'age':21, 'instrument':'clarinet'}
+sal={'age':18, 'instrument':'trombone'}
+fra={'age':40, 'instrument':'euphonium'}
+ant={'age':22, 'instrument':'trombone'}
+mik={'age':21, 'instrument':'sax'}
+people={'die':'Diego','nel':'Nello','sal':'Salvatore', 'fra':'francesco','mik':'Michele'}
+
+ins=raw_input("find_instrument ")
+
+
